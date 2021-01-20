07f0088c6fd88f5dc7800a832b600e5828cec3ec
Andrei Klimenok <jaguarislove@gmail.com>
2021-01-20 20:06:59 +0100

    Add 3rd task test

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,8 +1,17 @@
 import { parse, process } from '../src/generate';
 import { readFixtureFile } from '../src/io';
 
-test('Should generate correct output.json file', () => {
+const isArrayUnique = arr => new Set(arr).size === arr.length;
+
+describe('Generator results tests', () => {
   const result = process(parse(readFixtureFile('names.json')), parse(readFixtureFile('names1.json')));
-  const expected = parse(readFixtureFile('output.json'));
-  expect(result).toEqual(expected);
-});
+
+  it('Should generate correct result', () => {
+    const expected = parse(readFixtureFile('output.json'));
+    expect(result).toEqual(expected);
+  })
+
+  it.each(result.map(obj => obj["Names1"]))('Should not have duplicates in Names1', (...nameSet) => {
+    expect(isArrayUnique(nameSet)).toBeTruthy();
+  });
+})
